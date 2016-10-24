5b0fc8ba3ff9506d83a2e1b699e4f4b250258717
DimitarDKirov <dimkirov@gmail.com>
2016-10-24 03:08:25 +0300

    Added Homework solutions

---
+++ Topics/02. Processing-XML-in-.NET/homework/HW_XMLProcessing/2Artists/Program.cs
@@ -0,0 +1,45 @@
+﻿//2.Write program that extracts all different artists which are found in the catalog.xml.
+//    For each author you should print the number of albums in the catalogue.
+//    Use the DOM parser and a hash-table.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Artists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "..//..//catalog.xml";
+
+            var document = new XmlDocument();
+            document.Load(filePath);
+            var catalogRoot = document.DocumentElement;
+
+            IDictionary<string, int> albumsNumber = new Dictionary<string, int>();
+            foreach (XmlElement element in catalogRoot.ChildNodes)
+            {
+                string artist = element["artist"].InnerText;
+                if (!albumsNumber.ContainsKey(artist))
+                {
+                    albumsNumber.Add(artist, 1);
+                }
+                else
+                {
+                    albumsNumber[artist]++;
+                }
+            }
+
+            foreach (var artistAlbums in albumsNumber)
+            {
+                Console.WriteLine($"Artist {artistAlbums.Key} has {artistAlbums.Value} albums");
+            }
+        }
+    }
+}
---
+++ Topics/02. Processing-XML-in-.NET/homework/HW_XMLProcessing/5.ExtractTitles/Program.cs
@@ -0,0 +1,33 @@
+﻿//5.Write a program, which using XmlReader extracts all song titles from catalog.xml.
+
+using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace ExtractTitles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "..//..//catalog.xml";
+            IList<string> songs = new List<string>();
+            using (var reader = XmlReader.Create(filePath))
+            {
+                while (reader.Read())
+                {
+                    if (reader.Name != "song" || reader.NodeType != XmlNodeType.Element)
+                    {
+                        continue;
+                    }
+
+                    reader.ReadToDescendant("title");
+                    string songName = reader.ReadElementContentAsString();
+                    songs.Add(songName);
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, songs));
+        }
+    }
+}
